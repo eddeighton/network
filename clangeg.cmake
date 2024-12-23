0d534bfcf11a5baa9ab32ec0898b01a639631c03
Ed Deighton <eddeighton@hotmail.co.uk>
2024-12-23 01:46:43 +0000

    working link with clangeg in meta pipeline

---
+++ clangeg.cmake
@@ -0,0 +1,75 @@
+
+#########################################
+#########################################
+# resolve custom clang_eg build of llvm
+set( CLANG_EG_INSTALL_PATH /usr/local/clangeg )
+
+find_path( LLVM_DIR NAMES LLVMConfig.cmake PATHS ${CLANG_EG_INSTALL_PATH}/lib/cmake/llvm/ REQUIRED NO_DEFAULT_PATH)
+find_path( CLANG_DIR NAMES ClangConfig.cmake PATHS ${CLANG_EG_INSTALL_PATH}/lib/cmake/clang/ REQUIRED NO_DEFAULT_PATH)
+
+find_package( LLVM REQUIRED CONFIG )
+find_package( Clang REQUIRED CONFIG )
+
+#set(LLVM_LINK_COMPONENTS support)
+
+function( include_clang targetname )
+	target_compile_definitions( ${targetname} PUBLIC ${LLVM_DEFINITIONS} )
+	target_compile_definitions( ${targetname} PUBLIC ${CLANG_DEFINITIONS} )
+
+	target_include_directories( ${targetname} PUBLIC ${LLVM_INCLUDE_DIRS} )
+	target_include_directories( ${targetname} PUBLIC ${CLANG_INCLUDE_DIRS} )
+
+endfunction( include_clang )
+
+function( link_clang targetname )
+	include_clang( ${targetname} )
+
+	target_link_directories( ${targetname} PUBLIC ${LLVM_LIB_DIR} )
+	target_link_directories( ${targetname} PUBLIC ${CLANG_LIB_DIR} )
+
+    #target_link_libraries( ${targetname} ${llvm_libs} )
+	target_link_libraries( ${targetname} clangTooling clangEG )
+	
+endfunction( link_clang )
+
+function( link_clang_tooling targetname )
+	include_clang( ${targetname} )
+
+	target_link_directories( ${targetname} PUBLIC ${LLVM_LIB_DIR} )
+	target_link_directories( ${targetname} PUBLIC ${CLANG_LIB_DIR} )
+
+	llvm_map_components_to_libnames( llvm_tool_libs Support FrontendOpenMP )
+	#set(llvm_tool_libs)
+	Message(STATUS "llvm_tool_libs= ${llvm_tool_libs}")
+    target_link_libraries( ${targetname} ${llvm_tool_libs} )
+
+	target_link_libraries( ${targetname} 
+	
+		clangAST
+		clangASTMatchers
+		clangBasic
+		clangFrontend
+		clangSerialization
+		clangTooling
+	)
+	
+endfunction( link_clang_tooling )
+
+function( link_orc targetname )
+	include_clang( ${targetname} )
+
+	target_link_directories( ${targetname} PUBLIC ${LLVM_LIB_DIR} )
+	target_link_directories( ${targetname} PUBLIC ${CLANG_LIB_DIR} )
+
+	llvm_map_components_to_libnames( llvm_orc_libs core support codegen irreader x86codegen X86TargetMCA X86Desc X86Info LTO orcjit )
+	#set(llvm_libs)
+	Message(STATUS "llvm_orc_libs= ${llvm_orc_libs}")
+	target_link_libraries( ${targetname} ${llvm_orc_libs} )
+	
+endfunction( link_orc )
+
+function( import_clang targetname )
+	include_clang( ${targetname} )
+endfunction( import_clang )
+
+
--- meta/pipeline/task_test.cpp
+++ meta/pipeline/task_test.cpp
@@ -1,11 +1,106 @@
 
 
+#include "common/assert_verify.hpp"
 
+////////////////////////////////////////////
+#include "common/clang_warnings_begin.hpp"
 
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/AST/ASTConsumer.h"
 
+#include "clang/Basic/Thunk.h"
+#include "clang/AST/Mangle.h"
+
+#include "clang/Tooling/CompilationDatabase.h"
+#include "clang/Tooling/Tooling.h"
+
+#include "common/clang_warnings_end.hpp"
+////////////////////////////////////////////
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem/path.hpp>
 
 namespace mega::meta
 {
+
+namespace
+{
+using namespace llvm;
+using namespace clang;
+using namespace clang::tooling;
+using namespace clang::ast_matchers;
+using namespace std::string_literals;
+
+struct ToolDB : public clang::tooling::CompilationDatabase
+{
+    std::vector< clang::tooling::CompileCommand > m_commands;
+    std::vector< std::string >                    m_files;
+
+    ToolDB( const boost::filesystem::path& filePath )
+    {
+        m_files.push_back( filePath.string() );
+
+        clang::tooling::CompileCommand cmd;
+
+        cmd.Directory = filePath.parent_path().string();
+
+        /// The source file associated with the command.
+        cmd.Filename = filePath.filename().string();
+
+        /// The command line that was executed.
+        cmd.CommandLine = std::vector< std::string >{
+
+            "/build/linux_gcc_static_release/llvm/install/bin/clang-15"s,
+
+            "-resource-dir"s,
+            "/build/linux_gcc_static_release/llvm/install/lib/clang/15.0.0"s,
+
+            "-I"s,
+            "/workspace/root/src/common/src/api/"s,
+            "-I"s,
+            "/workspace/root/src/mega/src/include/"s,
+            "-I"s,
+            "/build/linux_gcc_shared_debug/boost/install/include/"s,
+
+            "-std=c++20"s,
+            "-fcoroutines-ts"s,
+            "-fgnuc-version=4.2.1"s,
+            "-fno-implicit-modules"s,
+            "-fcxx-exceptions"s,
+            "-fexceptions"s,
+            "-fcolor-diagnostics"s,
+            "-faddrsig"s,
+            "-D__GCC_HAVE_DWARF2_CFI_ASM=1"s,
+
+            "-Wno-unused-command-line-argument"s,
+
+            "-x"s,
+            "c++"s,
+            filePath.string(),
+
+        };
+
+        /// The output file associated with the command.
+        cmd.Output = ""s;
+
+        /// If this compile command was guessed rather than read from an authoritative
+        /// source, a short human-readable explanation.
+        /// e.g. "inferred from foo/bar.h".
+        cmd.Heuristic = ""s;
+
+        m_commands.push_back( cmd );
+    }
+    virtual std::vector< clang::tooling::CompileCommand > getCompileCommands( llvm::StringRef ) const
+    {
+        return m_commands;
+    }
+    virtual std::vector< std::string >    getAllFiles() const { return m_files; }
+    virtual std::vector< CompileCommand > getAllCompileCommands() const { return m_commands; }
+};
+
+}
+
 
 void task_test()
 {
