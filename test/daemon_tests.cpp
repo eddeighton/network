6e879cbcc6aa2cae00560f4a6b89e3f6627c8584
Ed Deighton <eddeighton@hotmail.co.uk>
2024-12-31 00:44:31 +0000

    start on service testing

--- test/daemon_tests.cpp
+++ test/daemon_tests.cpp
@@ -6,6 +6,9 @@
 #include "service/registry.hpp"
 #include "service/ptr.hpp"
 
+#include <boost/process.hpp>
+#include <boost/asio/io_service.hpp>
+
 #include <gtest/gtest.h>
 
 #include <utility>
@@ -16,6 +19,23 @@ TEST( Service, Daemon )
     using namespace mega::service;
     using namespace mega::test;
 
+    
+    namespace bp = boost::process;
 
+    std::future< std::string > daemonOutput, daemonError;
+    std::future< std::string > megaOutput,   megaError;
+
+    boost::asio::io_service ios;
+
+    bp::child procDaemon(   "/build/debug/bin/daemon",  bp::std_in.close(), bp::std_out > daemonOutput, bp::std_err > daemonError, ios );
+    bp::child procMega(     "/build/debug/bin/mega",    bp::std_in.close(), bp::std_out > megaOutput, bp::std_err > megaError, ios );
+
+    ios.run();
+
+    auto strDaemonOut = daemonOutput.get();
+    auto strMegaOut  = megaOutput.get();
+
+    std::cout << strDaemonOut << std::endl;
+    std::cout << strMegaOut << std::endl;
 }
 
